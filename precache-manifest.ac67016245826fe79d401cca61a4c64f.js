a521b2d305644d6754d463d6ea4ac569333bacb0
Krishna Vivek Kolasani <vivekkolasani45@gmail.com>
2020-09-13 15:59:53 +0530

    Updates

---
+++ precache-manifest.ac67016245826fe79d401cca61a4c64f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4762ee63d3671a229f790578bea4159",
+    "url": "/minimalclock/index.html"
+  },
+  {
+    "revision": "31fa20faee0fa4c32f1e",
+    "url": "/minimalclock/static/css/main.1e4f0468.chunk.css"
+  },
+  {
+    "revision": "cdfeb759bd409325aa1c",
+    "url": "/minimalclock/static/js/2.ed10e2cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minimalclock/static/js/2.ed10e2cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31fa20faee0fa4c32f1e",
+    "url": "/minimalclock/static/js/main.c6732b29.chunk.js"
+  },
+  {
+    "revision": "241d8ca5160b4ec5e536",
+    "url": "/minimalclock/static/js/runtime-main.2fbaca8f.js"
+  }
+]);
